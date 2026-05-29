87e82afac4c0cc4af8e5e8128bcc150b3f4d9002
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-05-29 11:52:21 -0300

    feat: add ensureAuthenticated middleware and extend Express Request Interface for user authentication

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { authConfig } from '@/config/auth';
+import { AppError } from '@/utils/AppError';
+
+interface ITokenPauload {
+  role: string
+  sub: string;
+}
+
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  try {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError('JWT token is missing', 401);
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as ITokenPauload;
+
+    request.user = {
+      id: user_id,
+      role,
+    }
+
+    return next();
+  } catch (error) {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
