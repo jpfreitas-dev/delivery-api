24d00b4f9de824eed29b22293399b3b7a238f1b6
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-05-29 11:15:31 -0300

    feat: implement user authentication and session management with JWT

--- src/controllers/uses-controllers.ts
+++ src/controllers/uses-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { hash } from 'bcrypt';
 import { z } from 'zod';
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/AppError';
 
 class UsersController {
   async create(request: Request, response: Response) {
@@ -12,9 +14,25 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError('User with same email already exists');
+    }
+
     const hashedPassword = await hash(password, 8);
 
-    return response.json({ message: 'User created successfully', hashedPassword });
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      }
+    })
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
