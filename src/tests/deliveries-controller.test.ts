7343e0e3ca7b34abd912097fffefce61f0c5bed6
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-05-30 12:21:25 -0300

    feat(tests): add DeliveriesController tests for creating deliveries

---
+++ src/tests/deliveries-controller.test.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { prisma } from "@/database/prisma";
+import { app } from "@/app";
+
+describe("DeliveriesController", () => {
+  let user_id: string;
+  let token: string;
+  let delivery_id: string;
+
+  afterAll(async () => {
+    await prisma.delivery.deleteMany({ where: { userId: user_id } });
+    await prisma.user.delete({ where: { id: user_id } })
+  })
+
+  it("should create a new delivery", async () => {
+    const userResponse = await request(app).post("/users").send({
+      name: "Delivery Test User",
+      email: "deliverytestuser@example.com",
+      password: "password123",
+    });
+
+    user_id = userResponse.body.id;
+
+    await prisma.user.update({
+      where: { id: user_id },
+      data: { role: "sale" },
+    });
+
+    const sessionResponse = await request(app).post("/sessions").send({
+      email: "deliverytestuser@example.com",
+      password: "password123",
+    });
+
+    token = sessionResponse.body.token;
+
+    const deliveryResponse = await request(app).post("/deliveries").send({
+      user_id,
+      description: "Test delivery description",
+    }).set("Authorization", `Bearer ${token}`);
+
+    expect(deliveryResponse.status).toBe(201);
+    expect(deliveryResponse.body.message).toBe("Create delivery");
+  })
+});
